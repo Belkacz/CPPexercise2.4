60f24c1da72d648c0bc9e51aa4746591febabb67
Łukasz Belka <lukasz.belka@fujitsu.com>
2023-05-30 16:48:42 +0200

    working on read file

--- zad2.4.cpp
+++ zad2.4.cpp
@@ -31,7 +31,7 @@ void wroteMatrix(int matrix[0][6], int N, string text){
     }
 }
 
-void saveToFile(int matrix[][6], int N){
+string saveToFile(int matrix[][6], int N){
     string filename;
     cout << "Podaj nazwę pliku: ";
     cin >> filename;
@@ -39,10 +39,42 @@ void saveToFile(int matrix[][6], int N){
     if (file.is_open()) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                file << " " << setw(2) << matrix[i][j] << " |";
+                file << matrix[i][j] << "|";
             }
             file << endl;
         }
+    } else {
+        cout << "Ups, coś poszło nie tak nie mogę otworzyć plisku" << endl;
+    }
+    file.close();
+    return filename;
+}
+
+void loadFromFile(int matrix[][6], int N, string filename){
+    ifstream file(filename);
+    int tempMatrix[6][6]; 
+    if (file.is_open()) {
+        int row = 0;
+        int column = 0;
+        while(!file.eof()){
+            cout << file.get();
+        if(file.get() != '|'){
+            row++;
+        }else{
+            file >> tempMatrix[column][row];
+        }
+        if(row >=6){
+            row = 0;
+            column ++;
+        }
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = tempMatrix[j][i];
+            }
+        }
+    } else {
+        cout << "Ups, coś poszło nie tak nie mogę otworzyć plisku" << endl;
     }
     file.close();
 }
@@ -51,13 +83,16 @@ void menu() {
     const int N = 6;
     int A[6][6];
     int minNum, maxNum;
+    string filename;
     srand(time(0));
 
     minNum = take("Podaj poczatek zakresu (liczbe minimalna): ");
     maxNum = take("Podaj koniec zakresu (liczbe maksymalna): ");
     randomData(N, A, minNum, maxNum);
     wroteMatrix(A, N, "macierz 6x6: ");
-    saveToFile(A, N);
+    filename = saveToFile(A, N);
+    loadFromFile(A, N, filename);
+    wroteMatrix(A, N, "macierz odwrocna 6x6: ");
 }
 
 int main() {
