dd13940464b8b53fc1c1629b2265f06b862aa401
Łukasz Belka <lukasz.belka@fujitsu.com>
2023-05-30 12:46:52 +0200

    save to file works, added pracownicy.txt for read from file in next step

--- zad2.4.cpp
+++ zad2.4.cpp
@@ -1,44 +1,66 @@
-// £ukasz Belka indeks nr. 156162
-
 #include <iostream>
 #include <ctime>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
-int take(string qestion){
+int take(string question) {
     int data;
-    cout << qestion << endl;
-    cin >> data >> endl;
-    return data
+    cout << question << endl;
+    cin >> data;
+    cout << endl;
+    return data;
 }
 
-
-void randomData(int N, int A[][N], int minNum, int maxNum){
-    for (int i = 0; i < numberOfYears; i++)
-    {
-        for (int j = 0; j < numberOfMonths; j++)
-        {
-            A[i][j] = rand() % maxNum + minNum;
+void randomData(int N, int A[][6], int minNum, int maxNum) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = rand() % (maxNum - minNum + 1) + minNum;
         }
     }
 }
-void menu(){
-    const int range = 6;
-    int N = 6;
-    int A[N][N];
-    int minNum, maxNum;
-    srand(time(0));
-    
-    int minNum = take("podaj poczatek zakresu (iczbe minimalna) : ");
-    int maxNum = take("podaj koniec zakresu (iczbe maksymalna) : ");
-    randomData(N, A, minNum, maxNum);
+
+void wroteMatrix(int matrix[0][6], int N, string text){
+    cout << text << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << " " << setw(2) << matrix[i][j] << " |";
+        }
+        cout << endl;
+    }
 }
 
+void saveToFile(int matrix[][6], int N){
+    string filename;
+    cout << "Podaj nazwę pliku: ";
+    cin >> filename;
+    ofstream file(filename);
+    if (file.is_open()) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                file << " " << setw(2) << matrix[i][j] << " |";
+            }
+            file << endl;
+        }
+    }
+    file.close();
+}
 
-int main(){
+void menu() {
+    const int N = 6;
+    int A[6][6];
+    int minNum, maxNum;
+    srand(time(0));
 
+    minNum = take("Podaj poczatek zakresu (liczbe minimalna): ");
+    maxNum = take("Podaj koniec zakresu (liczbe maksymalna): ");
+    randomData(N, A, minNum, maxNum);
+    wroteMatrix(A, N, "macierz 6x6: ");
+    saveToFile(A, N);
+}
+
+int main() {
     menu();
-
     return 0;
 }
